f261cd90fbe1b49048d5f955248a435ace9157ce
Kringlaren <adam.kring06@gmail.com>
2024-06-17 18:29:43 +0200

    Lägger till huvudmeny och options

---
+++ Scripts/Menus/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_up():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_quit_button_up():
+	get_tree().quit()
+
+
+func _on_options_button_up():
+	var options = preload("res://Scenes/options_menu.tscn").instantiate()
+	get_tree().get_root().add_child(options)
