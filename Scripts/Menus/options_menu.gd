e66bcd9acd51198d2451c4b971f5ddf47fd28acb
Kringlaren <adam.kring06@gmail.com>
2024-06-18 23:30:55 +0200

    Tog bort felaktig animation

--- Scripts/Menus/options_menu.gd
+++ Scripts/Menus/options_menu.gd
@@ -5,8 +5,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#spela padda up animationen
-	$AnimationPlayer.play("pad_animation")
-
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +15,4 @@ func _process(delta):
 
 func _on_button_button_up():
 	#spela animationen i en halv sekund och ta bort options från scenen
-	$AnimationPlayer.play_backwards("pad_animation")
-	await get_tree().create_timer(0.5).timeout
 	get_tree().root.remove_child(options_menu)
