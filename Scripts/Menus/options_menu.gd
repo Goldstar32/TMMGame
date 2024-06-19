66b82b6b7544c81d434156cab747002f13c86cc6
Scufydoodle <Simon.ekdebernardo@gmail.com>
2024-06-19 17:36:46 +0200

    Försökt göra volym ändringar till menyn

--- Scripts/Menus/options_menu.gd
+++ Scripts/Menus/options_menu.gd
@@ -1,17 +1,17 @@
 extends Control
 @onready var options_menu = $"."
 @onready var click_sound = preload("res://Assets/SFX/spacebar-click-keyboard-199448.mp3")
-
+@onready var sfx_bus = AudioServer.get_bus_index("SFX")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var sfx_slider = $MenuPanel/MarginContainer/VBoxContainer/GridContainer2/SFXSlider
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_button_button_up():
 	$SFX.stream = click_sound
@@ -21,6 +21,9 @@ func _on_button_button_up():
 
 
 func _on_option_button_item_selected(index):
+	$SFX.stream = click_sound
+	$SFX.play()
+	
 	#Ändrar skärmläge i spelet
 	match index:
 		0:
@@ -29,3 +32,9 @@ func _on_option_button_item_selected(index):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 		2:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+
+func _on_SFXSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
+	
