1a9239c491b1e2a4a03bd5059af3ad4345f1c522
Goldstar32 <josua.karnsund@gmail.com>
2024-06-24 22:50:00 +0200

    Structured player script

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 const SPEED : int = 150 # speed in pixels/sec
-
-func _physics_process(delta):
+func handle_input():
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
 
+
+func _physics_process(delta):
+	handle_input()
 	move_and_slide()
