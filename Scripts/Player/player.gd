1284097fe04114c0813dbf4e9214e24ffc2f33d2
Goldstar32 <josua.karnsund@gmail.com>
2024-06-24 22:52:10 +0200

    updated player script

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED : int = 150 # speed in pixels/sec
+@export var speed : int = 150 # speed in pixels/sec
+
 func handle_input():
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
+	velocity = direction * speed
 
 
 func _physics_process(delta):
