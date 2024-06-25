5889b746177e33963563c4c4fe520568b72cd5e7
Kringlaren <adam.kring06@gmail.com>
2024-06-25 17:36:37 +0200

    Working on and off pause menu without functionality

--- Scripts/AutoLoad/game_manager.gd
+++ Scripts/AutoLoad/game_manager.gd
@@ -1,27 +1,39 @@
 extends Node
 
+@onready var pause_menu = preload("res://Scenes/Menus/pause_menu.tscn")
+var pause_menu_instance : Control
+
 var pausable = true
 var cam : Camera2D
 
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _input(event : InputEvent):
 	if event.is_action_released("pause") and pausable:
+		if !pause_menu_instance:
+			pause_menu_instance = pause_menu.instantiate()
 		if get_tree().paused:
 			unpause()
 		else:
 			pause()
 		
 func pause():
-	get_tree().paused = true
-	
-	var pause_menu = preload("res://Scenes/Menus/pause_menu.tscn")
-	var pause_menu_instance : Control = pause_menu.instantiate()
 	if cam:
 		pause_menu_instance.scale = Vector2(cam.zoom[0] ** -1, cam.zoom[1] ** -1)
 		cam.add_child(pause_menu_instance)
 	else:
 		get_tree().root.add_child(pause_menu_instance)
+		
+	get_tree().paused = true
 func unpause():
-	get_tree().pasued = false
+	get_tree().paused = false
+	
+	if cam:
+		pause_menu_instance.queue_free()
+	else:
+		pause_menu_instance.queue_free()
 	
 	
 func set_cam(camera : Camera2D):
