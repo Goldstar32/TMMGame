d33b39377564a921c6b646bc87b033c50155c823
Kringlaren <adam.kring06@gmail.com>
2024-06-25 16:16:31 +0200

    Funcs in game manager

--- Scripts/AutoLoad/game_manager.gd
+++ Scripts/AutoLoad/game_manager.gd
@@ -9,3 +9,22 @@ func _input(event : InputEvent):
 			unpause()
 		else:
 			pause()
+		
+func pause():
+	get_tree().paused = true
+	
+	var pause_menu = preload("res://Scenes/Menus/pause_menu.tscn")
+	var pause_menu_instance : Control = pause_menu.instantiate()
+	if cam:
+		pause_menu_instance.scale = Vector2(cam.zoom[0] ** -1, cam.zoom[1] ** -1)
+		cam.add_child(pause_menu_instance)
+	else:
+		get_tree().root.add_child(pause_menu_instance)
+func unpause():
+	get_tree().pasued = false
+	
+	
+func set_cam(camera : Camera2D):
+	cam = camera
+func set_pausible(paus : bool):
+	pausable = paus
