6b52dc4b0729f4c91eff4d1b32c85d24ce26bcc5
Goldstar32 <josua.karnsund@gmail.com>
2024-06-27 18:13:30 +0200

    Fixed idle animation

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed : int = 120 # speed in pixels/sec
 @onready var animations = $AnimationPlayer
+var direction = "down" # direction in which the player is moving
 
 func handle_input():
 	var move_direction = Input.get_vector("left", "right", "up", "down")
@@ -9,11 +10,11 @@ func handle_input():
 
 func update_animation():
 	if velocity.length() == 0:
-		animations.play("idle_down")
+		animations.play("idle_" + direction)
 		animations.stop()
 	else:
-		var direction = "down"
-		if velocity.x < 0: direction = "down" # actually left
+		if velocity.y > 0: direction = "down"
+		elif velocity.x < 0: direction = "down" # actually left
 		elif velocity.x > 0: direction = "down" # actually right
 		elif velocity.y < 0: direction = "up"
 	
